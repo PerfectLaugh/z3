7735bd3f3b068e08e21c1c0536695c8a5615acf2
PerfectLaugh <me@perfectlaugh.info>
2025-11-14 17:40:41 +0800

    remove check on ocamlfind destdir directory
    This check will block dune package building due to dune sandboxing
    mechanism on ocamlfind.
    
    Better off disable it.

--- scripts/mk_util.py
+++ scripts/mk_util.py
@@ -2012,8 +2012,8 @@ class MLComponent(Component):
         if self.destdir == "":
             raise MKException('Failed to get OCaml destdir')
 
-        if not os.path.isdir(self.destdir):
-            raise MKException('The destdir reported by {ocamlfind} ({destdir}) does not exist'.format(ocamlfind=OCAMLFIND, destdir=self.destdir))
+        # if not os.path.isdir(self.destdir):
+        #     raise MKException('The destdir reported by {ocamlfind} ({destdir}) does not exist'.format(ocamlfind=OCAMLFIND, destdir=self.destdir))
 
         self.ldconf = check_output([OCAMLFIND, 'printconf', 'ldconf'])
         if self.ldconf == "":
